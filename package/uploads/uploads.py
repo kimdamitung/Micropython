945ca45fa1da5a367ecdf34304cb1aaef2a77fe1
kimdamitung <150353472+kimdamitung@users.noreply.github.com>
2024-09-23 12:22:29 +0700

    create uploads esp32 wroom

--- package/uploads/uploads.py
+++ package/uploads/uploads.py
@@ -3,6 +3,50 @@ import sys
 from time import time
 from colorama import Fore, Style
 
+def uploadsESP32WROOM(port):
+    start = time()
+    try:
+        uploads = [
+            'python', '-m', 'esptool',
+            '--chip', 'esp32',
+            '--port', port,
+            'erase_flash'
+        ]
+        with subprocess.Popen(uploads, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+            for line in process.stdout:
+                print(line, end='', flush=True)
+            stderr = process.stderr.read()
+            if stderr:
+                print(stderr, end='', flush=True)
+            process.wait()
+            if process.returncode != 0:
+                raise subprocess.CalledProcessError(process.returncode, uploads)
+        pushOS = [
+            'python', '-m', 'esptool',
+            '--chip', 'esp32',
+            '--port', port,
+            '--baud', '460800'
+            'write_flash', '-z', '0x1000', 'src/firmware/ESP32_GENERIC-20240602-v1.23.0.bin'
+        ]
+        with subprocess.Popen(pushOS, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+            for line in process.stdout:
+                print(line, end='', flush=True)
+            stderr = process.stderr.read()
+            if stderr:
+                print(stderr, end='', flush=True)
+            process.wait()
+            if process.returncode != 0:
+                raise subprocess.CalledProcessError(process.returncode, pushOS)
+        end = time()
+        total = end - start
+        print(f"\n========================= [{Fore.GREEN}SUCCESS{Fore.RESET}] Took {total:.2f} seconds =========================", flush=True)
+    except subprocess.CalledProcessError as e:
+        print("ERROR\n", e, flush=True)
+        print("ERROR\n", e.stderr, flush=True)
+        end = time()
+        total = end - start
+        print(f"\n========================= [{Fore.RED}FAILED{Fore.RESET}] Took {total:.2f} seconds =========================", flush=True)
+
 def uploadsESP32S3(port):
     start = time()
     try:
